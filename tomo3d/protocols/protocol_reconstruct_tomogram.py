d00bbc6ecea07ebed1b3e834b1c5877c8ef5925e
emma <emmaaire@gmail.com>
2026-05-14 10:40:03 +0200

    Revert "Bump version"
    This reverts commit 3531aaffc57b4620ef749b79606da0778346ea6d.

--- tomo3d/protocols/protocol_reconstruct_tomogram.py
+++ tomo3d/protocols/protocol_reconstruct_tomogram.py
@@ -266,7 +266,7 @@ class ProtTomo3dReconstrucTomo(ProtBaseTomo3d, ProtStreamingBase):
     def createOutStep(self, ts: TiltSeries):
         tsId = ts.getTsId()
         if tsId in self.failedItems:
-            self.addToOutFailedSet(ts)
+            self.addToOutFailedSet(tsId)
         else:
             inTsPointer = self.getInputTsSet(pointer=True)
             super().createOutputStep(inTsPointer, ts)
